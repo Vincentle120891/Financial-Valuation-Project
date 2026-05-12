35d0a21f2a0b8daa0c9a6355d2da090119f6fef2
Vincentle120891 <100365059+Vincentle120891@users.noreply.github.com>
2026-05-12 21:36:48 +0700

    Add files via upload

---
+++ frontend/src/components/valuation-flow/index.js
@@ -0,0 +1,30 @@
+ * Valuation Flow Components - Unified Exports
+ *
+ * Central export point for all reusable valuation flow components.
+ * These components follow the unified schema and support both
+ * International and Vietnamese markets.
+ */
+
+// Generic reusable components (Phase 2 - Component Simplification)
+export { default as GenericDataTable } from './GenericDataTable';
+export { default as MarketInfoCard } from './MarketInfoCard';
+export { default as DynamicFormGenerator } from './DynamicFormGenerator';
+
+// Legacy market-specific components (deprecated, use MarketInfoCard instead)
+// export { default as InternationalMarketData } from './InternationalMarketData';
+// export { default as VietnameseMarketData } from './VietnameseMarketData';
+
+// Step components (to be refactored in subsequent phases)
+export { default as ApiDataStep } from './ApiDataStep';
+export { default as AssumptionsStep } from './AssumptionsStep';
+export { default as BaselineStep } from './BaselineStep';
+export { default as CompanySelectionStep } from './CompanySelectionStep';
+export { default as DataReviewStep } from './DataReviewStep';
+export { default as ForecastDriversStep } from './ForecastDriversStep';
+export { default as HistoricalDataExtractionStep } from './HistoricalDataExtractionStep';
+export { default as ModelSelectionStep } from './ModelSelectionStep';
+export { default as PeerSelectionStep } from './PeerSelectionStep';
+export { default as RequirementsStep } from './RequirementsStep';
+export { default as ResultsStep } from './ResultsStep';
+export { default as RunValuationStep } from './RunValuationStep';
+export { default as SearchStep } from './SearchStep';
